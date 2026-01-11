23425b89f10e1335b5c15cac24c1aabce303a3d6
Leonardo F. dos Santos <44267124+qleonardolp@users.noreply.github.com>
2026-01-11 03:30:03 -0300

    Implement gravity comp + KD impedance

---
+++ simulink/hyl_fixed_setup.m
@@ -0,0 +1,9 @@
+%% HyL Simulink/Simscape Setup %%
+
+% Run this script to use HyL Simulink controller model.
+
+clear
+addpath('description','-end')
+HyLRigidBodyTree = importrobot('description\hyl_fixed_simulink.urdf', CollisionDecomposition=true);
+HyLRigidBodyTree.Gravity = [0 0 -9.80665];
+% show(HyLRigidBodyTree)
