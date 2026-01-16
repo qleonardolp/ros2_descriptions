6a90f55ee99cc0ae6b310fe28f77c7e970f195bc
Leonardo F. dos Santos <44267124+qleonardolp@users.noreply.github.com>
2026-01-16 01:50:22 -0300

    Add hip cylinder closed-chain kinematics

--- simulink/hyl_fixed_setup.m
+++ simulink/hyl_fixed_setup.m
@@ -1,9 +1,10 @@
 %% HyL Simulink/Simscape Setup %%
 
-% Run this script to use HyL Simulink controller model.
+% Run this script to generate the HyL RigidBodyTree struct for Simulink
 
 clear
 addpath('description','-end')
 HyLRigidBodyTree = importrobot('description\hyl_fixed_simulink.urdf', CollisionDecomposition=true);
 HyLRigidBodyTree.Gravity = [0 0 -9.80665];
 % show(HyLRigidBodyTree)
+clc
